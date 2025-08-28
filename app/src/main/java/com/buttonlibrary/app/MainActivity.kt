b1dcf6d9f09614178fbe8a50ed9822bcf83b0c5b
Jey <jeyvardanb2005@gmail.com>
2025-08-28 22:20:46 +0530

    Updated MainActivity.kt & ThreeDText.kt

--- app/src/main/java/com/buttonlibrary/app/MainActivity.kt
+++ app/src/main/java/com/buttonlibrary/app/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.tooling.preview.Preview
 import com.buttonlibrary.app.ui.theme.ButtonLibraryTheme
 import com.buttonlibrary.three_d_text.ThreeDText
@@ -17,38 +20,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ButtonLibraryTheme {
-                ThreeDText(
-                    text = "Sample Text",
-                    modifier = Modifier.wrapContentSize()
-                )
+                SampleUsageOfThreeDText()
             }
         }
     }
 }
 
-//@Composable
-//fun T() {
-//    androidx.compose.foundation.layout.Box(
-//        modifier = Modifier.fillMaxSize(),
-//        contentAlignment = Alignment.Center
-//    ) {
-//        T(
-//            text = " PRETTY \n COOL RIGHT !",
-//            modifier = Modifier.wrapContentSize(),
-//            baseColor = androidx.compose.ui.graphics.Color(0xFF000000),
-//            depthColor = androidx.compose.ui.graphics.Color(0xFFBEBEBE),
-//            depthOffset = Offset(1f, 1f),
-//            shadowColor = androidx.compose.ui.graphics.Color(0xFFC0C0C0),
-//            shadowOffset = Offset(3f, 3f),
-//            shadowBlurRadius = 1f,
-//        )
-//    }
-//}
+@Composable
+fun SampleUsageOfThreeDText() {
+    androidx.compose.foundation.layout.Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        ThreeDText(
+            text = "3D TEXT!",
+            modifier = Modifier.wrapContentSize(),
+            baseColor = androidx.compose.ui.graphics.Color(0xFFE7E5E5),
+            depthColor = androidx.compose.ui.graphics.Color(0xFF000000),
+            depthOffset = Offset(3f, 3f),
+            shadowColor = androidx.compose.ui.graphics.Color(0xFF000000),
+            shadowOffset = Offset(3f, 3f),
+            shadowBlurRadius = 1f,
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewText(){
     ButtonLibraryTheme {
-//        T()
+        SampleUsageOfThreeDText()
     }
 }
--- three-d-text/src/main/java/com/buttonlibrary/three_d_text/ThreeDText.kt
+++ three-d-text/src/main/java/com/buttonlibrary/three_d_text/ThreeDText.kt
@@ -2,9 +2,7 @@ package com.buttonlibrary.three_d_text
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -18,7 +16,6 @@ import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalTextApi::class)
@@ -86,3 +83,4 @@ fun ThreeDText(
             }
     )
 }
+
